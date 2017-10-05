5226985724e11a0c25ebb3ba6a16814c0a2cd997
vpsa23 <vpsa23@hotmail.com>
2017-10-05 00:42:15 -0300

    Cifrado Cesar

---
+++ README.md
@@ -0,0 +1,14 @@
+# *Ejercicio de Cifrado de César*
+
+Tuve que crear una que web que por medio de prompt(), ingresara un usuario alguna frase y que está devolviera el mismo mensaje pero Encriptado y tenía que tener un desplazamiento de 33 espacios hacia la derecha.
+
+### *Requerimientos Técnicos*
+
++ Dependencias de Desarrollo 
+  - Use un prompt para que le pudiera hacer una pregunta al usuario. 
+  - Hice la función de Encriptar para que el usuario colocalara una frase lo cual por medio de la formula (x-n) % 26 se la encriptara. 
+  - Realicé otra función para Desencriptar en la cual el usuario colocaba la palabra Desencriptada y esta le devolvia la palabra Encriptada para esto realicé la formula (valor ASCII -65 -7 + 26) %26 + 65 esta formula realizaba el proceso de la función.
+
+
+# *Diagrama de flujo*
+### Link: http://www.lucidchart.com/invitations/accept/d6b9c408-c975-461a-8fe7-7f8582cc3781
---
+++ cifrado.js
@@ -0,0 +1,49 @@
+// function cesar(phrase){
+	do{
+		var answer = prompt("Indique el número de lo que desea hacer: 1)Encriptar - 2)Descriptar");
+		if(answer != ""){
+			if(answer == "1") {
+				var phrase = prompt("Introduzca su palabra para Encriptar")
+				cipher(phrase);
+			} else if (answer == "2") {
+				var phrase = prompt("Introduzca su palabra para Desencriptar")
+				decipher(phrase);
+			} else {
+				alert("Ingrese una opción válida");
+			}
+		}
+	} while (answer == "" || (answer != "1" && answer != "2"));
+
+function cipher(phrase){
+	var result = "";
+	var  codigo= 0;
+
+	phrase = phrase.toUpperCase();
+
+
+	for (var i = 0; i<phrase.length; i++){
+		codigo = phrase.charCodeAt(i);
+		codigo = ((codigo-65+33)%26)+65;
+		result = result + String.fromCharCode(codigo);
+	}
+	phrase = phrase.toLowerCase();
+	alert(result);
+}
+
+
+function decipher(phrase){
+	var result = "";
+	var  codigo= 0;
+
+	phrase = phrase.toUpperCase();
+
+	for (var i = 0; i<phrase.length; i++){
+		codigo = phrase.charCodeAt(i);
+		codigo = ((codigo-65-7+26)%26+65);
+		result = result + String.fromCharCode(codigo);
+	}
+	phrase = phrase.toLowerCase();
+	alert(result);
+}
+
+
